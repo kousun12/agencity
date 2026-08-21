8299e38df529d9d6747180440d2e79e656d97fab
rob <kousun12@gmail.com>
2026-08-21 13:18:56 -0400

    chore: bump agencity source_ref to 4afa0c77

--- benchmarks/prime/agencity_verifiers/source.py
+++ benchmarks/prime/agencity_verifiers/source.py
@@ -1,3 +1,3 @@
 AGENCITY_SOURCE_BRANCH = "main"
 AGENCITY_SOURCE_REPO = "https://github.com/kousun12/agencity.git"
-AGENCITY_SOURCE_REF = "b96b0acf0403ab29ef53fb1e78213ff211f624ec"
+AGENCITY_SOURCE_REF = "4afa0c778cbe5fd4302a9528025631ceef2ab02c"
